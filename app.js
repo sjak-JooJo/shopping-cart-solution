356848503322d8d01b7416a9be09395de1dffd23
JooJo <sjak0101@gmail.com>
2022-05-26 17:58:13 +0100

    plust button fixed

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('I am in a separate file')
+function updateCaseNumber(isIncreasing){
+    const caseInput = document.getElementById('case-number');
+    let caseNumber = caseInput.value;
+    if(isIncreasing){ // if(isIncreasing == true)
+        caseNumber = parseInt(caseNumber) + 1;
+    }
+    else if(caseNumber > 0){
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+    // update case total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
+    
+}
+// case plus for increasing value
+document.getElementById('case-plus').addEventListener('click', function(){
+    updateCaseNumber(true);
+});
+// case minus for decreasing value
+document.getElementById('case-minus').addEventListener('click', function(){
+    updateCaseNumber(false);
+    
+});
